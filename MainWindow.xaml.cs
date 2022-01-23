6be51897ffc538ffe7e69c3311466a6374e04bca
Kirill Bolotin <dempi2016@yandex.ru>
2022-01-23 14:39:26 +0300

    Added a method to fill DataGrid with data from DataBase.

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -23,6 +25,32 @@ namespace PhoneBookApp
         public MainWindow()
         {
             InitializeComponent();
+            string sqlRequest = "SELECT * FROM [dbo].[PhoneBookTable]";
+            DataTable dt = Select(sqlRequest);
+            dgPhoneNumber.ItemsSource = dt.DefaultView; // Filling DataGrid with data from DataBase
+        }
+
+        public DataTable Select(string selectSQL) // Method to get data from DataBase
+        {
+            DataTable dataTable = new DataTable("dataBase");
+            SqlConnection sqlConnection = new SqlConnection("server=DESKTOP-7SHDMNV\\SQLEXPRESS;Trusted_Connection=Yes;DataBase=PhoneBook;");
+            sqlConnection.Open();
+            SqlCommand sqlCommand = sqlConnection.CreateCommand();
+            sqlCommand.CommandText = selectSQL;
+            SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(sqlCommand);
+            sqlDataAdapter.Fill(dataTable);
+            return dataTable;
+        }
+
+        private void ProfileButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void SettingsButton_Click(object sender, RoutedEventArgs e)
+        {
+
         }
     }
+
 }
